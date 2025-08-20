4b5b11668939357826f67b4ca01920c2f698dafb
khalid murtala <khalidmurtala81@gmail.com>
2025-08-20 05:12:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -1,41 +1,40 @@
-// hooks/use-local-storage.ts
+
 "use client"
 import { useState, useEffect } from 'react';
 
 function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
   const [storedValue, setStoredValue] = useState<T>(initialValue);
-  const [isMounted, setIsMounted] = useState(false);
-
+  
   useEffect(() => {
-    setIsMounted(true);
     try {
       if (typeof window !== 'undefined') {
         const item = window.localStorage.getItem(key);
-        setStoredValue(item ? JSON.parse(item) : initialValue);
+        if (item) {
+          setStoredValue(JSON.parse(item));
+        } else {
+          setStoredValue(initialValue);
+        }
       }
     } catch (error) {
       console.error(error);
       setStoredValue(initialValue);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [key]);
 
-  const setValue = (value: T) => {
-    if (typeof window === 'undefined') {
-        return;
-    }
+  const setValue = (value: T | ((val: T) => T)) => {
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       console.error(error);
     }
   };
-  
-  const clientValue = isMounted ? storedValue : initialValue;
 
-  return [clientValue, setValue];
+  return [storedValue, setValue];
 }
 
 export default useLocalStorage;
